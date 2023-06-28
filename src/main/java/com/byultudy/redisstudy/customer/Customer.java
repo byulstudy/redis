b6ce2bb08a2739439f7c4727a3a943f673603f50
im jiyoung <89112466+sazzeo@users.noreply.github.com>
2023-06-29 00:07:27 +0900

    Revert "V2/redisson"

--- src/main/java/com/byultudy/redisstudy/customer/Customer.java
+++ src/main/java/com/byultudy/redisstudy/customer/Customer.java
@@ -1,14 +1,8 @@
 package com.byultudy.redisstudy.customer;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+
+
 @Entity
 public class Customer {
 
@@ -16,6 +10,7 @@ public class Customer {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "customerId")
     private Long id;
+
     private String name;
 
 }
--- src/main/java/com/byultudy/redisstudy/ticket/Ticket.java
+++ src/main/java/com/byultudy/redisstudy/ticket/Ticket.java
@@ -1,32 +1,18 @@
 package com.byultudy.redisstudy.ticket;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 public class Ticket {
-    //예매후 티켓 발급함
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ticketId")
     private Long id;
 
-    private Long customerId;
+    private Long customerNo;
 
-    private Long concertId;
-
-    @Builder.Default
-    private boolean vip = false;
-
-    private LocalDateTime issueDate;
+    private Long convertNo;
 
 }
